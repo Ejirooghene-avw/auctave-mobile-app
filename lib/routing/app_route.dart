98e782c94e3b701e3400c409c826e9b7e04af158
Ejirooghene-avw <ejiroogheneav@gmail.com>
2026-01-06 14:03:30 +0100

    starting over

--- lib/routing/app_route.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppRoute {
-  static Map<String, WidgetBuilder> routes = {};
-}
--- lib/ui/onboarding/view_model/onboarding_view_model.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-class OnboardingViewModel extends ChangeNotifier {}
